17f54eeddc9dd0b7f32c6f7674b523820a0d493f
ncoder <bhargavayash2015@gmail.com>
2024-06-22 23:04:49 +0530

    remove product added

--- src/routes/adminRoutes.ts
+++ src/routes/adminRoutes.ts
@@ -1,10 +1,10 @@
 import express from 'express';
-import { addProduct } from '../controllers/adminControllers';
+import { addProduct, removeProduct } from '../controllers/adminControllers';
 import { protect } from '../middlewares/authMiddleware';
 const router = express.Router();
 router.route('/addProduct').post(protect,addProduct);
-// router.route('removeProduct').delete(protect,removeProduct);
-// router.route('/updateProduct').put(protect,updateProduct);
+router.route('/removeProduct/:id').delete(protect,removeProduct);
+// router.route('/updateProduct').patch(protect,updateProduct);
 // router.route('/getProducts').get(protect,getProducts);
 // router.route('/getProduct/:id').get(protect,getProduct);
 module.exports = router;
