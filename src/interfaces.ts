9e6f5420787d30881e28d6e1b7728f1da6426eb2
ncoder <bhargavayash2015@gmail.com>
2024-06-22 20:52:49 +0530

    null

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -3,32 +3,3 @@ export interface JWTUser {
     email: string;
     role: string;
 }
-
-export interface User {
-    id?: number;
-    createdAt?: Date;
-    email: string;
-    password: string;
-    name: string;
-}
-
-export interface Product {
-    id?: number;
-    createdAt?: Date;
-    updatedAt?: Date;
-    title: string;
-    price: number;
-    description: string;
-    color?: string;
-    owner?: Admin;
-    ownerId: number;
-}
-
-export interface Admin {
-    id?: number;
-    createdAt?: Date;
-    email: string;
-    password: string;
-    name: string;
-    products?: Product[];
-}
